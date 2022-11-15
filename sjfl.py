2b5f69720f13e4a9bd34e4c1c39510a1dea75de4
giligiliyes <245344724@qq.com>
2022-11-15 13:22:08 +0800

    s

---
+++ sjfl.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+import time
+import xlrd
+
+driver = webdriver.Chrome()
+# 窗口最大化
+driver.maximize_window()
+#读取文件
+data = xlrd.open_workbook("../sdsd/baidu.xls")
+#读取第一个工作表
+table = data.sheet_by_name('Sheet1')
+
+#使用for循环输出所有数据
+list=[]
+for i in range (0,table.nrows):
+    # 读取excel第一行
+    list = table.row_values(i)
+    break
+print(list)
+time.sleep(3)
+#通过索引在列表中获取百度URL
+driver.get(list[0])
+time.sleep(3)
+#通过索引在列表中获取百度输入框元素并输入要搜索的字段
+driver.find_element_by_id(list[3]).send_keys(list[4])
+time.sleep(3)
+#通过索引在列表中获取百度点击“百度一下”元素并点击
+driver.find_element_by_id(list[7]).click()
+time.sleep(3)
+list=[]
+for i in range (1,table.nrows):
+    # 读取excel第二行
+    list = table.row_values(i)
+    break
+print(list)
+#跳转到“百度首页”
+driver.find_element_by_css_selector(list[0]).click()
+time.sleep(2)
+#当Excel表中的数据是整数时，转换成列表后会有小数点，导致自动化运行失败，解决方法在脚本中用int（）转换成整数
+driver.find_element_by_id(list[1]).send_keys(int(list[2]))
+time.sleep(2)
+#点击搜索
+driver.find_element_by_id(list[3]).click()
+time.sleep(3)
+#关闭浏览器
+driver.quit()
+
+
+
+
+# driver.find_element_by_id("kw").send_keys("QQ",Keys.ENTER)
+
+
+
